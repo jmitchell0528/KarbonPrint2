0f3ec308e7329696c2c9354441058c1820bb3c86
jmitchell0528 <jlm0528@yahoo.com>
2017-08-03 00:45:27 -0500

    Finished basic css setup, now it's time to add the data

--- js/main-ctrl.js
+++ js/main-ctrl.js
@@ -3,7 +3,35 @@ angular.module('karbonPrint').controller('mainCtrl', function($scope, mainServic
   // $scope.test = "Hello! I'm working too!"
   mainService.getGame().then(function(result) {
     console.log(result)
-    $scope.games = result
-    
+    $scope.games = result;
+
+    $scope.navFunction = function() {
+      var x = document.getElementById('myTopnav');
+      if (x.className === "nav")  {
+        x.className += "responsive";
+      }
+      else  {
+        x.className = "nav";
+      }
+    }
+
+    var modal = document.getElementById('myModal');
+
+    $scope.modalBtn = function() {
+      var modalBtn = document.getElementsByClassName('search-position');
+      modal.style.display = "block";
+    }
+
+    $scope.span = function() {
+      var span = document.getElementsByClassName('close')[0];
+      modal.style.display = "none";
+    }
+
+    window.onclick = function(event)  {
+      if (event.target == modal)  {
+        modal.style.display = "none";
+      }
+    }
+
   })
 })
